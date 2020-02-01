1f62468cc80bcb63e8dbb4d8cbcabfe06e278390
Giuseppe Valentino <zepvalue@gmail.com>
2020-02-01 12:04:54 -0500

    added application component

--- src/components/Application/index.js
+++ src/components/Application/index.js
@@ -1,6 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import client from '../../modules/feathers';
 
-const Application = () => {};
+const Application = () => {
+  const [_messages, setMessages] = useState();
+  useEffect(() => {
+    const messages = client.service('messages');
+
+    // Add new messages to the message list
+    messages.on('created', message => setMessages(_messages.concat(message)));
+  }, []);
+
+  return <div>Board</div>;
+};
 
 export default Application;
