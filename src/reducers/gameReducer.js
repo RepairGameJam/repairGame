884abe9070b3f3ab250aa203cacf409658a76154
Ivan Felix <ivaneduardo68@gmail.com>
2020-02-01 13:12:26 -0500

    feat(gameReducer): create game reducer and actions creators

---
+++ src/reducers/gameReducer.js
@@ -0,0 +1,49 @@
+const initialState = {
+    score: 0,
+    requiredPieces: [],
+    level: {}
+}
+
+const START = 'START_GAME'
+const ADD_PIECE = 'ADD_PIECE'
+const ADD_SCORE = 'ADD_SCORE'
+
+export const startGameAction = level => ({
+    type: START,
+    level
+})
+
+export const addPieceAction = piece => ({
+    type: ADD_PIECE,
+    piece
+})
+
+export const addScoreAction = score => ({
+    type: ADD_SCORE,
+    score
+})
+
+const gameReducer = (state, action) => {
+    switch(action.type) {
+        case START:
+            return { 
+                score: 0,  
+                requiredPieces: [],
+                level: action.level
+            }
+        case ADD_SCORE: 
+            return {
+                ...state,
+                score: state.score + action.score
+            }
+        case ADD_PIECE:
+            return {
+                ...state,
+                requiredPieces: [state.requiredPieces, action.piece]
+            }
+        default:
+            return state
+    }
+}
+
+export default gameReducer
