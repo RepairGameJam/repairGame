93eeab95c7071e344dd65634d5ec4e00d21267a2
jchung201 <jchung201@gmail.com>
2020-02-02 10:56:37 -0500

    (Scoreboard) Connected scoreboard with store.

--- src/components/Application/content/PlayerScores.js
+++ src/components/Application/content/PlayerScores.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 const Scoreboard = styled.div`
   display: flex;
@@ -20,24 +20,22 @@ const PlayerTag = styled.p`
   text-align: center;
 `;
 
-const PlayerScores = ({ scores = [{ name: 'J', score: 100 }] }) => {
-  let topScores = scores.map( (x) => x );
-  topScores.sort((a,b) => b.score - a.score);
-  topScores = topScores.slice(0, 3);
+const PlayerScores = () => {
+  const players = useSelector(state => state.game.players);
+  const topScores = Object.keys(players).map(key => ({ name: key, score: players[key].score }));
+  topScores.sort((a, b) => b.score - a.score);
+  const finalTopScores = topScores.slice(0, 3);
   return (
     <Scoreboard>
-        {topScores.map(user => (
+      {finalTopScores.map(user => (
         <Player key={user.name}>
-            <PlayerTag>
+          <PlayerTag>
             {user.name}:{user.score}
-            </PlayerTag>
+          </PlayerTag>
         </Player>
-        ))}
+      ))}
     </Scoreboard>
-  );}
-
-PlayerScores.propTypes = {
-  scores: PropTypes.array.isRequired,
+  );
 };
 
 export default PlayerScores;
